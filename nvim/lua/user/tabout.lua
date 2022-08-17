f10ead73ba2c9a1cb475ad65a97d2eb4218ec8d5
Christoffer Andersson <chan8@kth.se>
2022-08-17 15:01:16 +0200

    added safety clause to tabout

--- nvim/lua/user/tabout.lua
+++ nvim/lua/user/tabout.lua
@@ -1,9 +1,9 @@
--- local status_ok, tabout = pcall(require, "tabout")
--- if not status_ok then
---     return
--- end
+local status_ok, tabout = pcall(require, "tabout")
+if not status_ok then
+    return
+end
 
-require('tabout').setup {
+tabout.setup {
     tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
     backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
     act_as_tab = true, -- shift content if tab out is not possible
@@ -23,4 +23,3 @@ require('tabout').setup {
     ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
     exclude = {} -- tabout will ignore these filetypes
 }
-
