8b414363ed2892e430d5d68b926165c46e2af156
Christoffer Andersson <chan8@kth.se>
2023-03-06 03:52:04 +0100

    added completion without typing

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -1,10 +1,16 @@
 local req = "cmp"
 local ok, cmp = pcall(require, req)
-if not ok then print("could not find require \"" .. req .. "\"") return end
+if not ok then
+    print("could not find require \"" .. req .. "\"")
+    return
+end
 
 local req = "luasnip"
 local ok, luasnip = pcall(require, req)
-if not ok then print("could not find require \"" .. req .. "\"") return end
+if not ok then
+    print("could not find require \"" .. req .. "\"")
+    return
+end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
@@ -15,10 +21,31 @@ end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-    Text = "", Method = "m", Function = "", Constructor = "", Field = "", Variable = "", Class = "",
-    Interface = "", Module = "", Property = "", Unit = "", Value = "", Enum = "", Keyword = "",
-    Snippet = "", Color = "", File = "", Reference = "", Folder = "", EnumMember = "", Constant = "",
-    Struct = "", Event = "", Operator = "", TypeParameter = ""
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -29,30 +56,33 @@ cmp.setup {
         end,
     },
     mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-d>"] = cmp.mapping { -- TODO  change back to <C-e>?
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        },
-        -- Accept currently selected item. If none selected, `select` first item.
+            ["<C-k>"] = cmp.mapping.select_prev_item(),
+            ["<C-j>"] = cmp.mapping.select_next_item(),
+            ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+            ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+            ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+            ["<C-d>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close(), },
+            ["<C-s>"] = cmp.mapping.complete(),
+        -- Acceptecurrently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_next_item()
-            elseif luasnip.expandable() then luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-            elseif check_backspace() then fallback()
-            else fallback()
+            ["<CR>"] = cmp.mapping.confirm { select = true },
+            ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
             end
         end, {
             "i",
             "s",
         }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -72,13 +102,13 @@ cmp.setup {
             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
             -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                nvim_lsp_signature_help = "[SIGNATURE]",
-                nvim_lua = "[NVIM_LUA]",
-                luasnip = "[Snippet]",
-                buffer = "[Buffer]",
-                path = "[Path]",
-            })[entry.source.name]
+                    nvim_lsp = "[LSP]",
+                    nvim_lsp_signature_help = "[SIGNATURE]",
+                    nvim_lua = "[NVIM_LUA]",
+                    luasnip = "[Snippet]",
+                    buffer = "[Buffer]",
+                    path = "[Path]",
+                })[entry.source.name]
             return vim_item
         end,
     },
